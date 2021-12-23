af22b93062df0377d725ea11df24ad3fed5c4231
云雀 <huafengcai@whale.im>
2021-12-23 14:06:02 +0800

    add singleFlight demo

--- 11-singleflight/demo.go
+++ 11-singleflight/demo.go
@@ -16,11 +16,26 @@ func main() {
 		})
 	}()
 	go func() {
+		g.Do("bbb", func() (interface{}, error) {
+			fmt.Println("bbb")
+			time.Sleep(1 * time.Second)
+			return nil, nil
+		})
+	}()
+	go func() {
 		g.Do("aaa", func() (interface{}, error) {
 			fmt.Println("aaaa")
 			time.Sleep(1 * time.Second)
 			return nil, nil
 		})
 	}()
+	go func() {
+		g.Do("ccc", func() (interface{}, error) {
+			fmt.Println("ccc")
+			time.Sleep(1 * time.Second)
+			return nil, nil
+		})
+	}()
+
 	select {}
 }
